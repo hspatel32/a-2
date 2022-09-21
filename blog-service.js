d3a5f22b53899a1bff69644633dd4059ec876caa
Harsh <hspatel32@myseneca.ca>
2022-09-21 19:13:41 -0400

    first commit

---
+++ blog-service.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+
+let posts = [];
+let categories = [];
+
+module.exports.initialize= function(){
+    return new Promise((resolve, reject)=>{
+        fs.readFile('./data/posts.json',(err,data)=>{
+            if(err){
+                reject(err);
+            }else{
+                posts = JSON.parse(data);
+                resolve();
+            };
+        });
+        fs.readFile('./data/categories.json',(err,data)=>{
+            if(err){
+                reject(err);
+            }else{
+                categories = JSON.parse(data);
+                resolve();
+            }
+        });
+    });
+};
+
+module.exports.getAllPosts = function(){
+    return new Promise((resolve,reject)=>{
+        if(posts.length==0){
+            reject("no data was returned");
+        }else{
+            resolve(posts);
+        }
+    });
+};
+
+module.exports.getPublishedPosts = function(){
+    return new Promise((resolve,reject)=>{
+        let filtPost = [];
+        for(let i=0;i< posts.length;i++){
+            if(posts[i].published == true){
+                filtPost.pust(posts[i]);
+            }
+        }
+        if(filtPost.length == 0){
+            reject("no data was returned");
+        }else{
+            resolve(filtPost);
+        }
+    });
+}
+
+module.exports.getCategories = function(){
+    return new Promise((resolve,reject)=>{
+        if(categories.length == 0){
+            reject("no data was returned");
+        }else{
+            resolve(categories);
+        }
+    });
+};
+
---
+++ server.js
@@ -0,0 +1,58 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Harsh Shaileshkumar Patel Student ID: 144938206 Date: 22-09-2022
+*
+*  Online (Cyclic) Link: ________________________________________________________
+*
+********************************************************************************/ 
+var express = require("express");
+var app = express();
+var path = require("path");
+const data = require("./blog-service.js");
+var HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+app.get('/', (req, res) => {
+    res.redirect("/about");
+});
+
+app.get("/about", function(req,res){
+    res.sendFile(path.join(__dirname,"views/about.html"));
+});
+
+app.get("/blog", function(req,res){
+    data.getAllPost().then((data)=>{
+        res.json(data);
+    });
+});
+
+app.get("/post", function(req,res){
+    data.getPublisgedPosts().then((data)=>{
+res.json(data);
+    });
+});
+
+app.get("/categories", (req,res)=>{
+    data.getCategories().then((data)=>{
+        res.jason(data);
+    });
+});
+
+app.use((req,res)=>{
+    res.status(404).send("Page Not Found");
+})
+
+data.initialize().then(function(){
+    app.listen(HTTP_PORT, onHttpStart);
+}).catch(function(err){
+    console.log("unable to start server: " + err);
+});
